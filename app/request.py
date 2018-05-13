284db800e561796bcc45351e07a93b2c54657156
victor njuguna <victormoore254@gmail.com>
2018-05-13 15:49:00 +0300

    write function to process results of the first API call

--- app/request.py
+++ app/request.py
@@ -27,3 +27,30 @@ def get_sources(country, category):
             source_results = process_sources(source_results_list)
 
     return source_results
+
+def process_sources(source_list):
+    '''
+    Function  that processes the news sources and transform them to a list of Objects
+
+    Args:
+        source_list: A list of dictionaries that contain news sources
+
+    Returns :
+        news_results: A list of news objects
+    '''
+    news_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        print(id)
+        name = source_item.get('name')
+        print(name)
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        country = source_item.get('country')
+        
+        if url:
+            source_object = NewsSource(id,name,description,url,category,country)
+            news_results.append(source_object)
+
+    return news_results
