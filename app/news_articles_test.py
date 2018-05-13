b6c176fd2343a8c909b11503ecc3f2c29412b723
victor njuguna <victormoore254@gmail.com>
2018-05-13 22:39:21 +0300

    write code to make the above test work

--- app/news_articles_test.py
+++ app/news_articles_test.py
@@ -13,7 +13,7 @@ class NewsArticlesTest(unittest.TestCase):
         '''
 
         self.new_article = NewsArticles("tmz","TMZ News", "Barbara Cohan","Social Media","Celebrity news",
-                                     "http://www.tmz.com","https://immage.tmdb.org/t/p/w500/khsjha27hbs" "2017-04-30T12:00:00Z")
+                                     "http://www.tmz.com","https://immage.tmdb.org/t/p/w500/khsjha27hbs", "2017-04-30T12:00:00Z")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article, NewsArticles))
