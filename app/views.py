cdc5096c9acfa05ece232a6173e727afbb3a2817
victor njuguna <victormoore254@gmail.com>
2018-05-13 13:43:48 +0300

    create root page function that returns the index page and its data

--- app/views.py
+++ app/views.py
@@ -8,4 +8,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'News-Highlights'
+    return render_template('index.html',message = message)
