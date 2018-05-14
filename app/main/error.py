7823fede9a44789705dbb4bd59dc25f414a13377
victor njuguna <victormoore254@gmail.com>
2018-05-14 14:00:00 +0300

    fixed some errors

--- app/main/error.py
+++ app/main/error.py
@@ -2,8 +2,8 @@ from flask import render_template
 from . import main
 
 @main.app_errorhandler(404)
-def four_Ow_four(error):
+def four_o_four(error):
     '''
     Function to render the 404 error page
     '''
-    return render_template('fourOwfour.html'),404
+    return render_template('four_o_four.html'),404
